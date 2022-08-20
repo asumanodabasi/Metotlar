e6d01155f809f3c91ddc5a4aa361dc340cd5e98f
asumanodabasi <asumanodabasi1@gmail.com>
2022-08-20 15:03:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunadi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            Product product1 = new Product();
+            product1.Adi = "Elma";
+            product1.Aciklama = "Amasya Elması";
+            product1.Fİyati = 15;
+
+            Product product2 = new Product();
+            product2.Adi = "Karpuz";
+            product2.Aciklama = "Diyarbakır Karpuzu";
+            product2.Fİyati = 80;
+
+            Product[] products = new Product[] { product1, product2 };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine(product.Fİyati);
+                Console.WriteLine("-------------------------");
+            }
+            Console.WriteLine("--------Metotlar-------------");
+            // instance - örnek
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(product1);
+            sepetManager.Ekle(product2);
+
+            sepetManager.Ekle2("Armut", "Yeşil", 12,10);
+            sepetManager.Ekle2("Elma", "Yeşil", 18,5);
+        }
+    }
+}
---
+++ SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+
+        public void Ekle(Product product)  //parametre verdik.Parametre demek ne eklemek istiyorsan onu ver demek.
+        {
+            Console.WriteLine("Tebrikler.Sepete Eklendi :" + product.Adi);
+        }
+        public void Ekle2(string urunadi,string aciklama,double fiyat,int stokadedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : "+urunadi);
+        }
+    }
+}
